6c4b9423bd4ae6368a1230dd23c08823bfb37e00
dabanya <33543459+dabanya@users.noreply.github.com>
2017-11-14 23:30:15 +0800

    Done

--- Test01.java
+++ Test01.java
@@ -1,43 +1,40 @@
-import java.awt.Insets;
 import java.awt.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test01 {
 	
-	static Scanner insert = new Scanner(System.in);
-	static int in = insert.nextInt();
-
 	public static void main(String[] args) {
 		
+		ArrayList list = new ArrayList(); 
+		list.add("anan"); list.add("安安");
+		
 		System.out.println("欢迎光临~\n请按照以下提示进行操作!\n查询请输入1 插入请输入2 修改请输入3 删除请输入4 清空请输入5 退出请输入0");
-				
-		while (in != 0) {
-			
-			System.out.println("查询请输入1   插入请输入2  修改请输入3   删除请输入4   清空请输入5   退出请输入0");
-			
-//			Scanner sc = new Scanner(System.in);
-//			String nu = sc.next();	
+		Scanner insert = new Scanner(System.in);
+		int in ;
+		in = insert.nextInt();
+		
+		while (in != 0) {			
 
-			ArrayList list = new ArrayList(); 
-			list.add("anan"); list.add("安安");
 
-			if ("1".equals(in)) {
+			if (1 == in) {
 				
-					for (int i = 0; i < list.size(); i++) {
-					String result = (String)list.get(i);
-					System.out.println(result +"   "+ i);				
+				for (int i = 0; i < list.size(); i++) {
+				String result = (String)list.get(i);
+				System.out.println(result +"   "+ i);	
+				System.out.println("-------------");
 				}
 				
-			} else if ("2".equals(in)) {
+			} else if (2 == in) {
 				
 				Scanner add = new Scanner(System.in);
 				System.out.println("请输入用户名：");
 				String name = add.next();
 
-				list.add(name);				
+				list.add(name);	
+				
 
-			}else if ("3".equals(in)) {
+			}else if (3 == in) {
 				
 				Scanner set = new Scanner(System.in);
 				System.out.println("请输入要修改的用户名ID：");
@@ -48,7 +45,7 @@ public class Test01 {
 				list.set(setID, newname);
 				
 				
-			}else if ("4".equals(in)) {
+			}else if (4 == in) {
 				
 				Scanner del = new Scanner(System.in);
 				System.out.println("请输入要删除的用户名ID：");
@@ -57,16 +54,19 @@ public class Test01 {
 				list.remove(delID);
 				
 				
-			}else if ("5".equals(in)) {
+			}else if (5 == in) {
 				
 				list.clear();
-				System.out.println("Clear successful~");
+				System.out.println("Clear Successful~");
 				
 			}
 			
-			
-			
+			System.out.println("查询请输入1   插入请输入2  修改请输入3   删除请输入4   清空请输入5   退出请输入0");
+			in = insert.nextInt();
+					
 		}
+		
+		System.out.println("结束~");
 	
 
 	}
